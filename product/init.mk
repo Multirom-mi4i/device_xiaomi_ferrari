0cc3ac118c3382d6b6f84118304cc5ac9c161edf
rohan <purohit.rohan@gmail.com>
2016-11-27 18:46:00 +0530

    ferrari: rootdir: cleanup and move to /etc
    * Add sensor permissions later

--- product/init.mk
+++ product/init.mk
@@ -1,11 +1,9 @@
 # Init scripts
 PRODUCT_PACKAGES += \
     fstab.qcom \
-    hsic.control.bt.sh \
-    init.class_main.sh \
-    init.qcom.early_boot.sh \
     init.qcom.fm.sh \
-    init.qcom.sh \
-    init.target.rc \
-    init.qcom.wifi.sh \
-    qca6234-service.sh
+    init.target.rc
+
+# Temp, until we find a better way
+PRODUCT_COPY_FILES += \
+    device/xiaomi/ferrari/rootdir/etc/ueventd.qcom.rc:root/ueventd.qcom.rc
---
+++ product/qcom-bluetooth.mk
@@ -0,0 +1,3 @@
+# Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.qualcomm.bt.hci_transport=0
